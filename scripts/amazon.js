dde46b48e6ba38ac3dae32423f9589bb04ff4672
Ashutosh Ranjan <authran.off@gmail.com>
2025-07-18 10:33:00 +0530

    .

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,38 +1,4 @@
-//We created this to understand the array we are creating
-// const products = [{
-//     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-//     rating: {
-//         stars: 4.5,
-//         count: 87
-//     },
-//     priceCent: 1090
-// }, {
-//     image: 'images/products/intermediate-composite-basketball.jpg',
-//     name: 'Intermediate Size Basketball',
-//     rating: {
-//         stars: 4,
-//         count: 127
-//     },
-//     priceCent: 2095
-// }, {
-//     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-//     rating: {
-//         stars: 4.5,
-//         count: 56
-//     },
-//     priceCent: 799
-
-// }, {
-//     image: 'images/products/black-2-slot-toaster.jpg',
-//     name: '2 Slot Toaster - Black',
-//     rating: {
-//         stars: 5,
-//         count: 2197
-//     },
-//     priceCent: 1899
-// }];
+import {cart} from '../data/cart.js'
 
 let productsHTML = '';
 
@@ -119,4 +85,42 @@ document.querySelectorAll('.js-add-to-cart')
       })
       document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     })
+ 
+ 
+    //We created this to understand the array we are creating
+  // const products = [{
+  //     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  //     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  //     rating: {
+  //         stars: 4.5,
+  //         count: 87
+  //     },
+  //     priceCent: 1090
+  // }, {
+  //     image: 'images/products/intermediate-composite-basketball.jpg',
+  //     name: 'Intermediate Size Basketball',
+  //     rating: {
+  //         stars: 4,
+  //         count: 127
+  //     },
+  //     priceCent: 2095
+  // }, {
+  //     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  //     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+  //     rating: {
+  //         stars: 4.5,
+  //         count: 56
+  //     },
+  //     priceCent: 799
+  
+  // }, {
+  //     image: 'images/products/black-2-slot-toaster.jpg',
+  //     name: '2 Slot Toaster - Black',
+  //     rating: {
+  //         stars: 5,
+  //         count: 2197
+  //     },
+  //     priceCent: 1899
+  // }];
+  
   })
