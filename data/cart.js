4fd05754f78289065f5979a49d0319ff0bba106f
Ashutosh Ranjan <authran.off@gmail.com>
2025-07-18 17:01:03 +0530

    Deletes product from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -7,3 +7,14 @@ export let cart = [{
 }];
 
 
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+}  
