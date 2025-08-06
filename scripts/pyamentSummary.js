fef8656e0d44fa0627ab453e5b6ce36ab3066dc4
Ashutosh Ranjan <authran.off@gmail.com>
2025-08-07 02:08:01 +0530

    Fully completing this E-commerce using pure js

--- scripts/pyamentSummary.js
+++ scripts/pyamentSummary.js
@@ -56,12 +56,27 @@ export function renderPaymentSummary() {
             <div class="payment-summary-money">$${(totalCents / 100).toFixed(2)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
 
         `
     document.querySelector('.js-payment-summary').innerHTML = paymmentSummaryHTML;
+
+    document.querySelector('.js-place-order')
+    .addEventListener('click', async() => {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method : "POST",
+        headers : {
+          'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+          cart : cart
+        })
+      })
+      const order = await response.json();
+      console.log(order); 
+    })
 
 };
 
